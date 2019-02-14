9fdcf31972a1e8e93a1bc4f1ded6fafb5c780da1
Mick Collins <collins@mandelbkCollins.lan>
2019-02-14 12:19:05 +1100

    buffered write statements

--- src/cutbonds/writecom.f
+++ src/cutbonds/writecom.f
@@ -19,7 +19,8 @@
 
       fact1=0.68d0
 
-      open(unit=20,file='OUT_ALLOC_FRAG',status='unknown')
+      open(unit=20,file='OUT_ALLOC_FRAG',status='unknown',
+     .             buffered='YES')
       write(20,*)' this files contains the list of atom numbers '
       write(20,*)' in the original molecule that'
       write(20,*)' correspond to the atomic coordinates in the '
@@ -52,7 +53,8 @@ c is missing from the junk array
 
       write(3,*)'The capping atoms are attached to'
 
-      open(unit=4,file='molecule.com',status='unknown')
+      open(unit=4,file='molecule.com',status='unknown',
+     .           buffered='YES')
 
 c this version accounts for charge
 c First find the net charge on the whole molecule
@@ -119,14 +121,16 @@ c     write(4,101)'--link1--'
 101   format(a9)
 
 c make a file that has the number of atoms in the fragments
-      open(unit=15,file='fragnum',status='unknown')
+      open(unit=15,file='fragnum',status='unknown',
+     .             buffered='YES')
       write(15,8765)ititle
 8765  format(1x,a20)
       numav=0
       numav2=0
       numcaps=0
 
-      open(unit=42,file='OUT_NCOORD_FRAG',status='unknown')
+      open(unit=42,file='OUT_NCOORD_FRAG',status='unknown',
+     .             buffered='YES')
       write(42,*)'Number of fragments is ',nffinal
 
       nemax=0.d0
@@ -336,7 +340,7 @@ c added extra 041218
         endif
       endif
 
-      open(unit=44,file=ca,status='unknown')
+      open(unit=44,file=ca,status='unknown',buffered='YES')
       write(44,*)itchg,',',ndegen
       do m=1,natf(k)
       if(fat(m).eq.'H ')then
